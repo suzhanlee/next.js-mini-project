9bd87382c55bfdc9368a7ce5298df776ecfd5f5d
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-11-17 01:18:51 +0900

    간단한 레이아웃 생성

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
+  let name = "lee"  
   return (
     <div> 
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev {name}</p>
     </div>
   );
 }
+
